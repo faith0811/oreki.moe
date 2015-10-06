8e2d696d8ed26456eecb11e57ecdc5c4f3acf827
Wang Yanqing <yanqing.wang@ele.me>
2015-10-07 01:02:35 +0800

    add static path

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = u'me'
 SITENAME = u'oreki.moe'
-SITEURL = ''
+SITEURL = u'oreki.moe'
 
 PATH = 'content'
 
@@ -12,24 +12,21 @@ TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = u'ch'
 
-# Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
          ('Jinja2', 'http://jinja.pocoo.org/'),
          ('You can modify those links in your config file', '#'),)
 
-# Social widget
 SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
 
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+STATIC_PATHS = ['images', 'extra/CNAME']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'}}
