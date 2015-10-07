5fd25871909df5d817623315465a6a18fcc7ecb4
Wang Yanqing <yanqing.wang@ele.me>
2015-10-07 13:46:43 +0800

    add some blog info

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = u'me'
+AUTHOR = u'折木 奉太郎'
 SITENAME = u'oreki.moe'
 SITEURL = u'http://oreki.moe'
 
@@ -11,7 +11,6 @@ PATH = 'content'
 TIMEZONE = 'Asia/Shanghai'
 
 DEFAULT_LANG = u'zh'
-LOCALE = u'zh_CN.utf-8'
 
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
@@ -19,17 +18,16 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = ((u'氷菓', 'http://www.kyotoanimation.co.jp/kotenbu/'),)
 
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Github', 'https://github.com/faith0811'),
+          ('Twitter', 'https://twitter.com/wyq0811'),)
 
 DEFAULT_PAGINATION = 10
 
 THEME = 'pelican-svbhack'
+TAGLINE = u'不做也行的事情就不做，非做不可的事情一切从简。'
+USER_LOGO_URL = SITEURL + '/images/oreki.jpeg'
 
 STATIC_PATHS = ['images', 'extra/CNAME']
 EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'}}
